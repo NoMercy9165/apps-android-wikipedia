70a80e4b6cb415af790c468f106f498231580385
NoMercy9165 <zeus.1991@list.ru>
2025-02-06 00:12:28 +0300

    Added locators for the onboarding screen

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework03/OnboardingScreen.kt
@@ -0,0 +1,63 @@
+package org.wikipedia.homeworks.homework03
+
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
+import org.wikipedia.views.AppTextView
+
+
+data class UiElement(
+    val viewClass: Class<*>,
+    val id: String,
+    val contentDescription: String? = null
+)
+
+val skipButton = UiElement(
+    viewClass = Button::class.java,
+    id = "fragment_onboarding_skip_button",
+    contentDescription = "onboarding_skip"
+)
+
+val continueButton = UiElement(
+    viewClass = Button::class.java,
+    id = "fragment_onboarding_forward_button",
+    contentDescription = "onboarding_continue"
+)
+
+val addLanguageButton = UiElement(
+    viewClass = Button::class.java,
+    id = "addLanguageButton",
+    contentDescription = "onboarding_multilingual_add_language_text"
+)
+
+val tabLayout = UiElement(
+    viewClass = TabLayout::class.java,
+    id = "view_onboarding_page_indicator"
+)
+
+val listLanguages = UiElement(
+    viewClass = RecyclerView::class.java,
+    id = "languagesList"
+)
+
+val centralLogo = UiElement(
+    viewClass = ImageView::class.java,
+    id = "imageViewCentered"
+)
+
+val headerUnderLogo = UiElement(
+    viewClass = AppTextView::class.java,
+    id = "primaryTextView"
+)
+
+val subtitle = UiElement(
+    viewClass = AppTextView::class.java,
+    id = "secondaryTextView",
+    contentDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+)
+
+val languageLabel = UiElement(
+    viewClass = AppTextView::class.java,
+    id = "option_label"
+)
